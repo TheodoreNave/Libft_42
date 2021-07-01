2e33cf9ae2f0f357df699ca596ebea3c1266f537
Théodore Nave <42880129+TheodoreNave@users.noreply.github.com>
2021-07-01 23:32:17 +0200

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"		// Deleted new_node == NULL for less lines
+#include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
